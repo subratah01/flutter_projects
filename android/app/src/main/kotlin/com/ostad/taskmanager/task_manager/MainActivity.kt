e83271198beaa606a2a0cb9c76416765349bffb5
subratah01 <subratah01@gmail.com>
2024-12-20 13:44:01 +0530

    Init Task Manager code

---
+++ android/app/src/main/kotlin/com/ostad/taskmanager/task_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ostad.taskmanager.task_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+  //Task Management Project Using REST API and Flutter
+  runApp(const TaskManagerApp());
+}
+
+
