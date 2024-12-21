d9e4cd44fc03703ee29639f4b46baaea1bc2b1cc
subratah01 <subratah01@gmail.com>
2024-12-21 14:00:34 +0530

    Splash screen and sign-in screen initiated

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/sign_in_screen.dart';
+import 'package:task_manager/ui/screens/splash_screen.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager',
+      initialRoute: '/',
+      onGenerateRoute: (RouteSettings settings) {
+        late Widget widget;
+        if (settings.name == SplashScreen.name) {
+          widget = const SplashScreen();
+        }
+        else if (settings.name == SignInScreen.name) {
+          widget = const SignInScreen();
+        }
+        return MaterialPageRoute(builder: (_) => widget);
+      },
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+            minimumSize: const Size(350, 45),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/screens/sign_in_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  static const String name = '/sign-in';
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //ElevatedButton(onPressed: (){}, child: const Text('Sign in'))
+            ],
+          ),
+        ),),
+    );
+  }
+}
